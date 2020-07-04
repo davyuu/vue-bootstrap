54f1b1ed7b68fa7771c37008f04df082a6ea535e
David <davidyu_2005@hotmail.com>
2020-07-04 17:32:21 -0400

    Create mock server

---
+++ mock/src/routes/notes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const uuid = require('uuid/v1');
+const { notes } = require('../state');
+
+const router = express.Router()
+
+router.get('/', (_, res) => {
+  return res.status(200).send(notes);
+})
+
+router.post('/', (req, res) => {
+  const { note } = req.body;
+  const newNote = {
+    ...note,
+    id: uuid()
+  }
+
+  if (Math.random() > 0.2) {
+    notes.push(newNote)
+    return res.status(200).send(newNote)
+  }
+  return res.status(500).send()
+})
+
+module.exports = router
