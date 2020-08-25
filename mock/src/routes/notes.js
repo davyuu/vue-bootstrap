f9d4081f8bb3891ad6c069bb2219d5748bde0aa7
David <davidyu_2005@hotmail.com>
2020-08-24 20:24:41 -0400

    Fix npm audit

--- mock/src/routes/notes.js
+++ mock/src/routes/notes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const uuid = require('uuid/v1');
+const { v1: uuid } = require('uuid');
 const { notes } = require('../state');
 
 const router = express.Router()
